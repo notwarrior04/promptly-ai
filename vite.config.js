ea0210de17fb9ebf1f464ad428f05d9a1937c51d
notwarrior04 <subhoprasad04@gmail.com>
2025-08-05 21:20:33 +0530

    Integrated voice input/output and Speak buttons

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/promptly-ai/', // 👈 this line is required for GitHub Pages
   plugins: [react()],
 })
